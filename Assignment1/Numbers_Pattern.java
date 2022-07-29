3aa516ebde5d5af598d058d3729a6c571ea677e8
Kranthi Puttapaka <64770733+kranthi575@users.noreply.github.com>
2022-07-29 13:40:57 +0530

    Add files via upload

---
+++ Assignment1/Numbers_Pattern.java
@@ -0,0 +1,16 @@
+
+public class Numbers_Pattern{
+	
+	public static void main(String args[]) {
+		
+		for(int i=1;i<5;i++) {
+			
+			for(int j=0;j<4;j++) {
+				System.out.print(i);
+				
+			}
+			System.out.println();
+		}
+	}
+
+}
